dfb4d09d72b210bba3b908270ce5c9af5874be27
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-08-23 15:05:24 +0530

    ✅ 3Sum | Medium | Python | Runtime: 0 ms | Memory: 19.2 MB | 2026-08-23

---
+++ medium/0015-3sum/solution.py
@@ -0,0 +1,7 @@
+                    right-=1
+                    while left<right and nums[left]==nums[left-1]:
+                        left+=1
+                    while left<right and nums[right]==nums[right+1]:
+                elif s<0:
+                        right-=1
+
