82fd3ab1d001aa74c5b233354ff30a6c32d288f1
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-08-23 15:42:19 +0530

    ✅ 4Sum | Medium | Python | Runtime: 400 ms (49.61%) | Memory: 19.2 MB | 2026-08-23

--- medium/0018-4sum/solution.py
+++ medium/0018-4sum/solution.py
@@ -1,31 +1,30 @@
 class Solution:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
-        r=[]
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        s=0
+        r=[]
         for i in range(len(nums)):
-            if i>0 and nums[i]==nums[i-1]:
+            if i>0 and  nums[i]==nums[i-1]:
                 continue
-            left=i+1
-            right=len(nums)-1
-            while left<right:
-                s=nums[i]+nums[left]+nums[right]
-                if s==0:
-                    r.append([nums[i],nums[left],nums[right]])
-                    left+=1
-                    right-=1
-                    while left<right and nums[left]==nums[left-1]:
+            for j in range(i+1,len(nums)):
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                left=j+1
+                right=len(nums)-1
+                while left<right:
+                    s=nums[i]+nums[j]+nums[left]+nums[right]
+                    if s==target:
+                        r.append([nums[i],nums[j],nums[left],nums[right]])
                         left+=1
-                    while left<right and nums[right]==nums[right+1]:
+                        right-=1
+                        while left<right and  nums[left]==nums[left-1]:
+                            left+=1
+                        while left<right and  nums[right]==nums[right+1]:
+                            right-=1
+                        
+                    elif s<target:
+                        left+=1
+
+                    else:
                         right-=1
 
-                elif s<0:
-                    left+=1
-                else:
-                    right-=1
         return r
-                
-        
-
-
-        
