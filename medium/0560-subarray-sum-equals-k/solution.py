088e173c3a8b949791a4d77d77d2a0426306914d
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-08-30 16:09:57 +0530

    ✅ Subarray Sum Equals K | Medium | Python | Runtime: 30 ms (76.47%) | Memory: 21.8 MB | 2026-08-30

--- medium/0560-subarray-sum-equals-k/solution.py
+++ medium/0560-subarray-sum-equals-k/solution.py
@@ -1,12 +1,12 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left=0
-        right=len(numbers)-1
-        while left<right:
-            s=numbers[left]+numbers[right]
-            if s== target:
-                return[left+1,right+1]
-            elif s<target:
-                left+=1
-            else:
-                right-=1
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count=0
+        prefix=0
+        seen={0:1}
+        for num in nums:
+            prefix+=num
+            if prefix-k in seen:
+                count+=seen[prefix-k]
+            seen[prefix]=seen.get(prefix,0)+1
+        return count
+        
