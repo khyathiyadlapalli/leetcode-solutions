7933d2fde0f51c893859653b1ce64efb99a7ffd0
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-08-30 15:18:49 +0530

    ✅ Two Sum II - Input Array Is Sorted | Medium | Python | Runtime: 0 ms | Memory: 19.3 MB | 2026-08-30

--- medium/0167-two-sum-ii-input-array-is-sorted/solution.py
+++ medium/0167-two-sum-ii-input-array-is-sorted/solution.py
@@ -1,7 +1,7 @@
-        for i in range(len(numbers)-1):
-            for j in range(i+1,len(numbers)):
-                if numbers[i]+numbers[j]!=target:
-                return[i+1,j+1]
+class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-                    continue
+        for i in range(1,len(numbers)-1):
+            for j in range(i+1,len(numbers)-1):
+                if numbers[i]+numbers[j]==target:
+                    return[i,j]
         
