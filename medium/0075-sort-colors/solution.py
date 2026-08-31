2d0eb3c0203dd8dad99753c6da6f76b0f453a675
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-08-31 17:31:58 +0530

    ✅ Sort Colors | Medium | Python | Runtime: 0 ms | Memory: 19.3 MB | 2026-08-31

--- medium/0075-sort-colors/solution.py
+++ medium/0075-sort-colors/solution.py
@@ -1,11 +1,6 @@
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        i=0
-        j=0
-        while i<len(s) and j<len(t):
-            if s[i]==t[j]:
-                i+=1
-            j+=1
-        return i==len(s)
-        
-        
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        return nums.sort()
