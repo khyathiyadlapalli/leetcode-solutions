3cb5c5c1cf31abfe0436b3098a589f7b7d1969fe
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-08-31 15:29:39 +0530

    ✅ Is Subsequence | Easy | Python | Runtime: 0 ms | Memory: 19.4 MB | 2026-08-31

--- easy/0392-is-subsequence/solution.py
+++ easy/0392-is-subsequence/solution.py
@@ -1,7 +1,7 @@
         j=0
         while i<len(s) and j<len(t):
             if s[i]==t[j]:
-                i+1
+                i+=1
             j+=1
         
-        return True
+        return i==len(s)
