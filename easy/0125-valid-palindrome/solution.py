90fcfd1e60057387c7c316899e941beddfb497f9
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-08-31 12:42:24 +0530

    ✅ Valid Palindrome | Easy | Python | Runtime: 10 ms (36.34%) | Memory: 19.4 MB | 2026-08-31

--- easy/0125-valid-palindrome/solution.py
+++ easy/0125-valid-palindrome/solution.py
@@ -1,12 +1,9 @@
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        slow=0
-        for fast in range(len(nums)):
-            if nums[fast]!=0:
-                nums[slow],nums[fast]=nums[fast],nums[slow]
-                slow+=1
+    def isPalindrome(self, s: str) -> bool:
 
-        return[nums]
+        clean=""
+        for ch in s:
+            if ch.isalnum():
+                clean+=ch.lower()
+        return clean==clean[::-1]
+        
