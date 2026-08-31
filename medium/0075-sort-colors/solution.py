12b4cc02459fa0d6eea864eccbc14fc15786142f
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-08-31 17:34:53 +0530

    ✅ Sort Colors | Medium | Python | Runtime: 0 ms (100.00%) | Memory: 19.3 MB | 2026-08-31

--- medium/0075-sort-colors/solution.py
+++ medium/0075-sort-colors/solution.py
@@ -3,4 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        low=0
+        mid=0
+        hight=len(nums)-1
+        while mid<=hight:
+            if nums[mid]==0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[hight]=nums[hight],nums[mid]
+                hight-=1
+                
