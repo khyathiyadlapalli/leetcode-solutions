8afc190cb3eb3c7ab078dce38c4773d7c053dcf0
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-08-31 14:56:28 +0530

    ✅ Remove Duplicates from Sorted Array | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 20.5 MB | 2026-08-31

--- easy/0026-remove-duplicates-from-sorted-array/solution.py
+++ easy/0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,18 +1,12 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        i=m-1
-        j=n-1
-        k=n+m-1
-        while j>=0:
-            if i>=0 and nums1[i]>nums2[j]:
-                nums1[k]=nums1[i]
-                i-=1
-            else:
-                nums1[k]=nums2[j]
-                j-=1
-            k-=1
-        return nums1
-        
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        slow = 0
+
+        for fast in range(1, len(nums)):
+
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+
+        return slow + 1
