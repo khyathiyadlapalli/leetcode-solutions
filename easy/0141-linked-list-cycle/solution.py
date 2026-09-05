57f4e5de5ba05a32df997b40f3563f0ca589d7d1
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-09-05 16:49:12 +0530

    ✅ Linked List Cycle | Easy | Python | Runtime: 53 ms (61.33%) | Memory: 22.4 MB | 2026-09-05

--- easy/0141-linked-list-cycle/solution.py
+++ easy/0141-linked-list-cycle/solution.py
@@ -1,16 +1,17 @@
-class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        left=0
-        right=len(height)-1
-        max_area=0
-        while left<right:
-            width=right-left
-            h=min(height[left],height[right])
-            area=width*h
-            max_area=max(area,max_area)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-            if height[left]<height[right]:
-                left+=1
-            else:
-                right-=1
-        return max_area
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
+                return True
+        return False
+        
