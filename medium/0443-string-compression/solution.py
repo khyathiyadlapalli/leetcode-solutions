97185740e4c4d2b5ab6ef0b362465a626e29aded
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-09-05 13:14:23 +0530

    ✅ String Compression | Medium | Python | Runtime: 1 ms (58.61%) | Memory: 19.5 MB | 2026-09-05

--- medium/0443-string-compression/solution.py
+++ medium/0443-string-compression/solution.py
@@ -1,13 +1,19 @@
 class Solution:
-    def numRescueBoats(self, people: List[int], limit: int) -> int:
-        people.sort()
-        left=0
-        right=len(people)-1
-        boats=0
-        while left<=right:
-            if people[left]+people[right]<=limit:
-                left+=1
-            right-=1
-            boats+=1
-        return boats
-        
+    def compress(self, chars: List[str]) -> int:
+        read=0
+        write=0
+        while read<len(chars):
+            current= chars[read]
+            count=0
+            while read<len(chars) and chars[read]==current:
+                read+=1
+                count+=1
+
+            chars[write]=current
+            write+=1
+
+            if count>1:
+                for digit in str(count):
+                    chars[write]=digit
+                    write+=1
+        return write
