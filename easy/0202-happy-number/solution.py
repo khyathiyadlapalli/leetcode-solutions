ddc7fba7d79635df8dcf9e81152e363041179145
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-09-05 18:45:34 +0530

    ✅ Happy Number | Easy | Python | Runtime: 0 ms | Memory: 19.2 MB | 2026-09-05

--- easy/0202-happy-number/solution.py
+++ easy/0202-happy-number/solution.py
@@ -1,35 +1,25 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
+    def isHappy(self, n: int) -> bool:
 
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
-            return True
+        slow = n
+        fast = n
 
-        slow=head
-        fast=head
-        while fast and fast.next:
-            slow=slow.next
-            fast=fast.next.next
-        curr=slow
-        prev=None
-        while curr:
-            next_node=curr.next
-            curr.next=prev
-            prev=curr
-            curr=next_node
+        while fast != 1:
 
-        left=head
-        right=prev
-        while right:
-            if left.val!=right.val:
-                
+            slow = self.getNext(slow)
+            fast = self.getNext(self.getNext(fast))
+
+            if slow == fast:
                 return False
-            left=left.next
-            right=right.next
+
         return True
 
-        
+    def getNext(self, n):
+        total = 0
+
+        while n > 0:
+            digit = n % 10
+            total += digit * digit
+            n = n // 10
+
+        return total
