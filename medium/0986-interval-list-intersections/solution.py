44984b0506e79f056ec1d38b7af0022af002de46
Khyathi_Yadlapalli <93853415+khyathiyadlapalli@users.noreply.github.com>
2026-09-05 14:11:30 +0530

    ✅ Interval List Intersections | Medium | Python | Runtime: 2 ms (84.03%) | Memory: 20 MB | 2026-09-05

--- medium/0986-interval-list-intersections/solution.py
+++ medium/0986-interval-list-intersections/solution.py
@@ -1,27 +1,16 @@
 class Solution:
-    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
-        tokens.sort()
-
-        left = 0
-        right = len(tokens) - 1
-        score = 0
-        max_score = 0
-
-        while left <= right:
-            if power >= tokens[left]:
-                # Face-up: spend power, gain score
-                power -= tokens[left]
-                left += 1
-                score += 1
-                max_score = max(max_score, score)
-
-            elif score > 0:
-                # Face-down: spend score, gain power
-                power += tokens[right]
-                right -= 1
-                score -= 1
-
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        i=0
+        j=0
+        r=[]
+        while i<len(firstList) and j<len(secondList):
+            start=max(firstList[i][0],secondList[j][0])
+            end=min(firstList[i][1],secondList[j][1])
+            if start<=end:
+                r.append([start,end])
+            if firstList[i][1]<secondList[j][1]:
+                i+=1
             else:
-                break
-
-        return max_score
+                j+=1
+        return r
+        
